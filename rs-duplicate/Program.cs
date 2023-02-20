2a74fcbc01dc1358038f2c05b8e6aada5cdde049
Pavel Ibrahim <bavelibrahim@gmail.com>
2023-02-20 23:45:37 +0100

    Removed Code on main...

--- rs-duplicate/Program.cs
+++ rs-duplicate/Program.cs
@@ -8,19 +8,7 @@ namespace RsDuplicate
         static void Main(string[] args)
         {
 
-            Warrior warrior = new("Elian");
-
-            int TotalAttributes = warrior.TotalAttributes();
-            Console.WriteLine("TotalAttributes before: " + TotalAttributes);
-
-            HeroAttributes attributes = new(1, 2, 2);
-
-            Armor armorPlate = new("Elians Armor", 1, Slots.Head, attributes, ArmorTypes.Plate);
-
-            warrior.EquipArmor(armorPlate);
-
-            TotalAttributes = warrior.TotalAttributes();
-            Console.WriteLine("TotalAttributes After Equipping armor: " + TotalAttributes);
+            //---------------
 
         }
     }
